852b3ac6560349bfdbe3df7c528e941270085ae4
Kaan Chinar <128641309+kaanchinar@users.noreply.github.com>
2025-01-21 13:21:57 +0400

    rewrite login/register mechanism

---
+++ AddressBookFinal/Models/Contact.cs
@@ -0,0 +1,13 @@
+﻿namespace AddressBook.Models;
+
+public class Contact
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    
+    public int UserId { get; set; }
+    public virtual User User { get; set; }
+}
---
+++ AddressBookFinal/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace AddressBook.Models;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; }
+    public string PasswordHash { get; set; }
+    public string Email { get; set; }
+    public virtual ICollection<Contact> Contacts { get; set; }
+}
